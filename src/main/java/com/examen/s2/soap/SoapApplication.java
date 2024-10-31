4a53dfd01d14026410e9625ae4526f7b4fa2e206
EdiFarfan <efarfanr@miumg.edu.gt>
2024-10-31 09:40:22 -0600

    Empezando

---
+++ src/main/java/com/examen/s2/soap/SoapApplication.java
@@ -0,0 +1,13 @@
+package com.examen.s2.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApplication.class, args);
+	}
+
+}
