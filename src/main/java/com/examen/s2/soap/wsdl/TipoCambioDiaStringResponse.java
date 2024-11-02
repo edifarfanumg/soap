1d4d2488c5b4934138cb2bd9bfddab012a862d5a
EdiFarfan <efarfanr@miumg.edu.gt>
2024-11-01 19:56:14 -0600

    problemas aun no solucionados

--- src/main/java/com/examen/s2/soap/wsdl/TipoCambioDiaStringResponse.java
+++ src/main/java/com/examen/s2/soap/wsdl/TipoCambioDiaStringResponse.java
@@ -4,12 +4,9 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "TipoCambioDiaStringResponse")
 public class TipoCambioDiaStringResponse {
-    // Tus atributos y métodos aquí
 
-    // Ejemplo de un atributo
     private String resultado;
 
-    // Getters y setters
     public String getResultado() {
         return resultado;
     }
